8aa36aaf8cde08d16f8f6092030ada5008fce993
ThierryRakotomanana <ThierryRktm@gmail.com>
2025-11-08 15:27:47 +0300

    feat(api): get user's location

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
+import * as z from 'zod'
 import './App.css'
 
+const userLocationSchema = z.string()
+
 function App() {
+  const [location, setLocation] = useState<z.infer<typeof userLocationSchema>>()
+
+  const getUserLocation = async(githubUsername : string): Promise<string> => {
+  const response = await fetch(`https://api.github.com/users/${githubUsername}`)
+  if (!response.ok) throw new Error('Network error')
+  const { location } = await response.json()
+  const raw = z.parse(userLocationSchema, location)
+  setLocation(raw)
+  return raw
+  }
 
   return (
     <>
+    <button onClick={()=> getUserLocation(`ThierryRakotomanana`)}>Get Location</button>
+    <div> {location} </div>
     </>
   )
 }
