f1f6e8aca61a339fd9a107db31563c210d4e662b
ThierryRakotomanana <ThierryRktm@gmail.com>
2025-11-13 22:04:04 +0300

    feat(api): add fetchAllPage to get all user's followers & following

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { type GithubUser, type GithubLocation } from './types/api.types'
-import { fetchGithubUserData, fetchUserLocation } from './api/github'
+import { fetchAllPages, fetchUserLocation } from './api/github'
 
 function App() {
   const [location, setLocation] = useState<GithubLocation>()
   const [userFollowers, setUserFollowers] = useState<GithubUser[]>()
   const [userFollowing, setUserFollowing] = useState<GithubUser[]>()
 
-  const getUserData = async (call : string) => {
-    const result = await fetchGithubUserData(call)
+  const getUserData = async (call : 'followers'|'following') => {
+    const result = await fetchAllPages(call)
     call == `followers` ? setUserFollowers(result) : setUserFollowing(result)
     
   }
--- src/api/github.ts
+++ src/api/github.ts
@@ -9,9 +9,25 @@ export const fetchUserLocation = async(githubUsername : string): Promise<GithubL
   return z.parse(LocationSchema, location)
 }
 
-export const fetchGithubUserData = async (dataType : string) : Promise<GithubUser[]> => {
-    const response = await fetch(`${GITHUB_CONFIGURATION.apiBase}/users/ThierryRakotomanana/${dataType}?per_page=100`)
+export const fetchGithubUserData = async (RequestType : 'followers'|'following', page : number) : Promise<GithubUser[]> => {
+    const response = await fetch(`${GITHUB_CONFIGURATION.apiBase}/users/ThierryRakotomanana/${RequestType}?per_page=100&page=${page}`)
     if (!response.ok) throw Error("Newtork problem")
     const rawData = await response.json()
     return z.array(GithubUserSchema).parse(rawData);
 }
+
+export const fetchAllPages = async(RequestType : 'followers'|'following') : Promise<GithubUser[]> => {
+  let calling = true
+  let page = 1, Users : GithubUser[] = []
+  while(calling) {
+    const audience = await fetchGithubUserData(RequestType, page)
+    Users = [...Users, ...audience]
+    if(audience.length == 0 ){ 
+      calling = false
+    }
+    console.log(page)
+    page++
+  }
+  console.dir(Users)
+  return Users
+} 
