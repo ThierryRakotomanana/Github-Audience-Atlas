ec5eba886234d6bacc10322598bf8d181cca12e5
ThierryRakotomanana <ThierryRktm@gmail.com>
2025-11-15 14:01:52 +0300

    style(format): apply prettier defined style

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,53 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { type GithubUser } from './types/api.types'
-import { fetchAllPages } from './api/github'
-import { GithubExplorer } from './components/GithubExplorer'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { type GithubUser } from './types/api.types';
+import { fetchAllPages } from './api/github';
+import { GithubExplorer } from './components/GithubExplorer';
 
 function App() {
-  const [userFollowers, setUserFollowers] = useState<GithubUser[]>()
-  const [userFollowing, setUserFollowing] = useState<GithubUser[]>()
-  const [ghosts, setGhosts] = useState<GithubUser[]>()
+  const [userFollowers, setUserFollowers] = useState<GithubUser[]>();
+  const [userFollowing, setUserFollowing] = useState<GithubUser[]>();
+  const [ghosts, setGhosts] = useState<GithubUser[]>();
 
   useEffect(() => {
     async function fechAudience() {
       const [followers, following] = await Promise.all([
         await fetchAllPages('followers'),
-        await fetchAllPages('following')
-      ])
-      setUserFollowers(followers)
-      setUserFollowing(following)
-      
-      const isGhost = new Map<number, GithubUser>()
+        await fetchAllPages('following'),
+      ]);
+      setUserFollowers(followers);
+      setUserFollowing(following);
 
-      followers.map((follower)=> {
-        isGhost.set(follower.id, follower)
-      })
+      const isGhost = new Map<number, GithubUser>();
+
+      followers.map((follower) => {
+        isGhost.set(follower.id, follower);
+      });
 
       setGhosts(() => {
-        const ghosts = following.filter((following)=> {
-          if (!isGhost.has(following.id)) return following
-        })
-        return ghosts
-      })
+        const ghosts = following.filter((following) => {
+          if (!isGhost.has(following.id)) return following;
+        });
+        return ghosts;
+      });
     }
-    fechAudience()
-  },[])
+    fechAudience();
+  }, []);
 
   return (
     <>
-    <div> { userFollowers && userFollowing && ghosts && <GithubExplorer followers={userFollowers} following={userFollowing} ghosts={ghosts}/>}
-    </div>
+      <div>
+        {' '}
+        {userFollowers && userFollowing && ghosts && (
+          <GithubExplorer
+            followers={userFollowers}
+            following={userFollowing}
+            ghosts={ghosts}
+          />
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/api/github.ts
+++ src/api/github.ts
@@ -1,28 +1,46 @@
-import { z } from "zod"
-import { GithubUserSchema, LocationSchema, type GithubLocation, type GithubUser, type AudienceType } from "../types/api.types"
-import { GITHUB_CONFIGURATION } from "../config"
+import { z } from 'zod';
+import {
+  GithubUserSchema,
+  LocationSchema,
+  type GithubLocation,
+  type GithubUser,
+  type AudienceType,
+} from '../types/api.types';
+import { GITHUB_CONFIGURATION } from '../config';
 
-export const fetchUserLocation = async(githubUsername : string): Promise<GithubLocation> => {
-  const response = await fetch(`${GITHUB_CONFIGURATION.apiBase}/users/${githubUsername}`)
-  if (!response.ok) throw new Error('Network error')
-  const { location } = await response.json()
-  return z.parse(LocationSchema, location)
-}
+export const fetchUserLocation = async (
+  githubUsername: string,
+): Promise<GithubLocation> => {
+  const response = await fetch(
+    `${GITHUB_CONFIGURATION.apiBase}/users/${githubUsername}`,
+  );
+  if (!response.ok) throw new Error('Network error');
+  const { location } = await response.json();
+  return z.parse(LocationSchema, location);
+};
 
-export const fetchGithubUserData = async (audienceType : AudienceType, page : number) : Promise<GithubUser[]> => {
-    const response = await fetch(`${GITHUB_CONFIGURATION.apiBase}/users/ThierryRakotomanana/${audienceType}?per_page=100&page=${page}`)
-    if (!response.ok) throw Error("Newtork problem")
-    const rawData = await response.json()
-    return z.array(GithubUserSchema).parse(rawData);
-}
+export const fetchGithubUserData = async (
+  audienceType: AudienceType,
+  page: number,
+): Promise<GithubUser[]> => {
+  const response = await fetch(
+    `${GITHUB_CONFIGURATION.apiBase}/users/ThierryRakotomanana/${audienceType}?per_page=100&page=${page}`,
+  );
+  if (!response.ok) throw Error('Newtork problem');
+  const rawData = await response.json();
+  return z.array(GithubUserSchema).parse(rawData);
+};
 
-export const fetchAllPages = async(audienceType : AudienceType) : Promise<GithubUser[]> => {
-  let page = 1, allAudiences : GithubUser[] = []
-  while(true) {
-    const audienceByPage = await fetchGithubUserData(audienceType, page)
-    allAudiences = [...allAudiences, ...audienceByPage]
-    if(audienceByPage.length == 0 || audienceByPage.length < 100 ) break
-    page++
+export const fetchAllPages = async (
+  audienceType: AudienceType,
+): Promise<GithubUser[]> => {
+  let page = 1,
+    allAudiences: GithubUser[] = [];
+  while (true) {
+    const audienceByPage = await fetchGithubUserData(audienceType, page);
+    allAudiences = [...allAudiences, ...audienceByPage];
+    if (audienceByPage.length == 0 || audienceByPage.length < 100) break;
+    page++;
   }
-  return allAudiences
-} 
+  return allAudiences;
+};
