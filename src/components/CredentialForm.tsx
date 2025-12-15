a6e0693539e703a0b318eb470c03b80d6c3dfb64
ThierryRakotomanana <ThierryRktm@gmail.com>
2025-12-15 12:11:38 +0300

    style: update text colors

--- src/components/CredentialForm.tsx
+++ src/components/CredentialForm.tsx
@@ -15,22 +15,20 @@ export default function CredentialForm({
 	const [showToken, setShowToken] = useState<boolean>(false);
 
 	return (
-		<div className='min-h-svh flex flex-col items-center justify-center bg-background gap-4'>
-			<div className='flex flex-col gap-3 justify-center mb-6 text-center'>
+		<div className='min-h-svh flex flex-col items-center justify-center atlas-grid-bg  gap-4'>
+			<div className='flex flex-col gap-5 justify-center mb-6 text-center'>
 				<div className='text-5xl'>🌍</div>
-				<h1 className='text-3xl font-extrabold tracking-tight bg-magic-trinity bg-clip-text text-transparent'>
-					AUDIENCE ATLAS
-				</h1>
-				<p className='text-xs text-muted-foreground'>
-					followers · following · ghost zone
-				</p>
+				<div>
+					<h1 className='text-3xl font-extrabold atlas-brand-text'>
+						AUDIENCE ATLAS
+					</h1>
+					<p className='text-xs'>followers · following · ghost zone</p>
+				</div>
 			</div>
 			<Card className='w-full max-w-md border-border shadow-lg font-light p-4'>
 				<CardContent className='pt-4 pb-0 flex flex-col gap-5'>
 					<div className='flex flex-col gap-2'>
-						<Label
-							htmlFor='user'
-							className='text-xs font-medium text-muted-foreground'>
+						<Label htmlFor='user' className='text-xs font-light'>
 							GitHub Username
 						</Label>
 						<div className='relative'>
@@ -41,7 +39,6 @@ export default function CredentialForm({
 								placeholder='e.g. torvalds'
 								value={form.user}
 								onChange={(e) => setForm((f) => ({ ...f, user: e.target.value }))}
-								className='pl-9 font-mono text-sm focus-visible:ring-primary'
 								autoComplete='off'
 								spellCheck={false}
 							/>
@@ -50,9 +47,7 @@ export default function CredentialForm({
 
 					<div className='flex flex-col gap-2'>
 						<div className='flex justify-between'>
-							<Label
-								htmlFor='token'
-								className='text-xs font-medium text-muted-foreground'>
+							<Label htmlFor='token' className='text-xs font-light'>
 								Personal Access Token
 							</Label>
 							<Label className='text-xs font-medium text-muted-foreground'>
@@ -61,21 +56,19 @@ export default function CredentialForm({
 						</div>
 
 						<div className='relative text-muted-foreground'>
-							<Button
+							<button
 								onClick={() => {
 									setShowToken((show) => !show);
 								}}
-								variant={"ghost"}
-								className='absolute left-0 hover:bg-none top-1/2 -translate-y-1/2 flex items-center'>
+								className='absolute left-3 top-1/2 translate-x- -translate-y-1/2 w-4 h-4 hover:bg-none flex items-center'>
 								{showToken ? "👁" : "🙈"}
-							</Button>
+							</button>
 							<Input
 								id='token'
 								type={showToken ? "text" : "password"}
 								placeholder={"ghp_xxxxxxxxxxxx"}
 								value={form.token}
 								onChange={(e) => setForm((f) => ({ ...f, token: e.target.value }))}
-								className='pl-9 font-mono text-sm focus-visible:ring-primary'
 								autoComplete='off'
 								spellCheck={false}
 							/>
@@ -83,36 +76,38 @@ export default function CredentialForm({
 					</div>
 
 					<Button
-						className='w-full mt-1 bg-blue-800 hover:bg-primary/90 text-primary-foreground font-medium p-5'
+						className='w-full mt-1 bg-blue-800 hover:bg-primary/90 text-slate-100 font-medium p-5'
 						disabled={!valid}
 						onClick={() => valid && onSubmit(form)}>
 						Generate Atlas →
 					</Button>
 
-					<p className='text-center text-xs text-muted-foreground leading-relaxed border-t border-border pt-4'>
+					<p className='text-center text-xs text-foreground leading-relaxed border-t border-border pt-4'>
 						Token requires{" "}
-						<code className='bg-secondary text-indigo-900 px-1 py-0.5 rounded text-[11px]'>
+						<code className='bg-background text-indigo-500 px-1 py-0.5 rounded text-[11px]'>
 							read:user
 						</code>{" "}
 						and{" "}
-						<code className='bg-secondary text-purple-950 px-1 py-0.5 rounded text-[11px]'>
+						<code className='bg-background text-purple-500 px-1 py-0.5 rounded text-[11px]'>
 							read:followers
 						</code>{" "}
-						scopes. <br />
-						<a
-							href='https://github.com/settings/tokens'
-							className='text-primary hover:underline'
-							target='_blank'
-							rel='noopener noreferrer'>
-							Generate one →
-						</a>
-						<p className='mt-3'>
+						scopes.
+						<div>
+							<a
+								href='https://github.com/settings/tokens'
+								className='text-primary hover:underline mt-2'
+								target='_blank'
+								rel='noopener noreferrer'>
+								Generate one →
+							</a>
+						</div>
+						<p className='mt-2'>
 							✓ Add{" "}
-							<code className='bg-secondary text-blue-600 px-1 py-0.5 rounded text-[11px] mt-2'>
+							<code className='bg-background text-blue-400 px-1 py-0.5 rounded text-[11px] mt-2'>
 								user:follow
 							</code>{" "}
 							scope to unlock{" "}
-							<code className='bg-secondary text-green-600 px-1 py-0.5 rounded text-[11px]'>
+							<code className='bg-background text-green-400 px-1 py-0.5 rounded text-[11px]'>
 								Unfollow buttons
 							</code>{" "}
 							scopes.
