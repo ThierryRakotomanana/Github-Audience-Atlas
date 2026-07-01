141cc569388aa149b9dce96a31bb4b03acc25186
ThierryRakotomanana <ThierryRktm@gmail.com>
2026-07-01 20:24:14 +0300

    refactor: revert to the app layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { WorldMap } from "@/components/WorldMap.tsx";
+import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<WorldMap width={900} height={450} />
+		<App />
 	</StrictMode>
 );
